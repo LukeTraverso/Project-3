a4e37e94130f455dad7d63f4b1cdddd2b92c30b5
LukeTraverso <traverso.luke@student.ccm.edu>
2024-12-06 23:37:03 -0500

    Randolph's Pizza Reviews

---
+++ yelpkeys.py
@@ -0,0 +1,16 @@
+# Luke Traverso
+# Professor Binowski
+# Data Science Programming
+# 20 November 2024
+"""
+API Keys for yelp Fusion API for Review Sentiment App
+"""
+"""
+My own keys:
+
+client_id="zox8QcSllyTh-pJwYPvjLQ"
+api_key="1ZZ8h63cSmqCottDtvbHBvk_NWnwxNIx-Ul_pnDPHMH57xWeEIhFnx_1azfHkcKF5NLQLTcHAWmt2XAN-Ib8u0fZcosF119UpcBct3sgqAJ2s7eOpKpN-I0bBQ0-Z3Yx"
+"""
+# Professor's Keys
+client_id="istl9jgw2i0Hp64EIdomvQ"
+api_key="9F_s1ES7zWFSsWT8SlhxAhOVK-Tk_5jGtGgyiJhnct4cDXrGdjAMkMdmmeYTqRdVTEJXbWTPCdAh1fas6JAkQrYLhZ9rWbsVXlhIePicia5XV1UOcb3BryAjn986ZHYx"
